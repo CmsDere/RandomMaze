c9fe8c796cf0db4fb5f29cc791cab8a84356d6c5
CmsDere <airbusboeing3076@gmail.com>
2024-04-16 01:18:32 +0900

    commit

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -46,6 +46,9 @@ public class MazeGenerator : MonoBehaviour
 
     void CreateCell(int x, int y, int z)
     {
+        float distance = cellPrefab.transform.lossyScale.x / 2;
+        Vector3 wallHeight = new Vector3(0, wallPrefab.transform.lossyScale.y / 2, 0);
+
         if (!cellInfo[x, y, z])
         {
             return;
@@ -54,14 +57,58 @@ public class MazeGenerator : MonoBehaviour
         {
             // 瘤痢 积己
             cellObjects[x, y, z] = Instantiate
-                (
-                    cellPrefab,
-                    transform.TransformDirection(new Vector3(x, y, z)),
-                    Quaternion.identity,
-                    transform
-                );
-            // 寒 积己
-            
+            (
+                cellPrefab,
+                transform.TransformDirection(new Vector3(x, y, z)),
+                Quaternion.identity,
+                transform
+            );  
         }
+
+        Vector3 basePos = transform.TransformDirection(cellObjects[x, y, z].transform.position);
+
+        for (int d = 0; d < (int)DIRECTION.MAX; d++)
+        {
+            if (!wallInfo[x, y, z, d])
+            {
+                return;
+            }
+        }
+        // 合率 寒 积己
+        wallObjects[x, y, z, (int)DIRECTION.NORTH] = Instantiate
+            (
+                wallPrefab,
+                basePos + (Vector3.forward * distance) + wallHeight,
+                Quaternion.Euler(0, 90, 0),
+                cellObjects[x, y, z].transform
+            );
+        wallObjects[x, y, z, (int)DIRECTION.NORTH].name = $"Stage {y+1} North Wall ({x}, {z})";
+        // 巢率 寒 积己
+        wallObjects[x, y, z, (int)DIRECTION.SOUTH] = Instantiate
+            (
+                wallPrefab,
+                basePos + (Vector3.back * distance) + wallHeight,
+                Quaternion.Euler(0, 90, 0),
+                cellObjects[x, y, z].transform
+            );
+        wallObjects[x, y, z, (int)DIRECTION.SOUTH].name = $"Stage {y + 1} South Wall ({x}, {z})";
+        // 辑率 寒 积己
+        wallObjects[x, y, z, (int)DIRECTION.WEST] = Instantiate
+            (
+                wallPrefab,
+                basePos + (Vector3.forward * distance) + wallHeight,
+                Quaternion.identity,
+                cellObjects[x, y, z].transform
+            );
+        wallObjects[x, y, z, (int)DIRECTION.WEST].name = $"Stage {y + 1} West Wall ({x}, {z})";
+        // 悼率 寒 积己
+        wallObjects[x, y, z, (int)DIRECTION.EAST] = Instantiate
+            (
+                wallPrefab,
+                basePos + (Vector3.forward * distance) + wallHeight,
+                Quaternion.identity,
+                cellObjects[x, y, z].transform
+            );
+        wallObjects[x, y, z, (int)DIRECTION.EAST].name = $"Stage {y + 1} East Wall ({x}, {z})";
     }
 }
