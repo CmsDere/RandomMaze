a660384dbd676754314065d136865f47d014b1e2
CmsDere <airbusboeing3076@gmail.com>
2024-07-28 11:55:33 +0900

    24072801

--- Assets/1. Scripts/Treasure/TreasureBox.cs
+++ Assets/1. Scripts/Treasure/TreasureBox.cs
@@ -8,15 +8,15 @@ public class TreasureBox : MonoBehaviour
     public bool isSelect = false;
     bool isExecute = false;
 
-    Renderer renderers;
-
     Material outlineMat;
-    Material originalMat;
+    [SerializeField] Material originalMat;
+
+    Animator animator;
 
     void Start()
     {
-        renderers = GetComponent<Renderer>();
         outlineMat = new Material(Shader.Find("Custom/Outline"));
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,15 +26,21 @@ public class TreasureBox : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 // 아이템 획득
+                GetItem();
             }
         }
     }
 
     public void SelectBox()
     {
+        Transform[] allChild = GetComponentsInChildren<Transform>();
+
         if (isSelect && !isExecute)
         {
-            renderers.material = outlineMat;
+            foreach(Transform child in allChild)
+            {
+                child.gameObject.GetComponent<Renderer>().material = outlineMat;
+            }
             UIManager.instance.OpenUI(UIType.InteractUI);
             isExecute = true;
         }
@@ -42,9 +48,14 @@ public class TreasureBox : MonoBehaviour
 
     public void DeselectBox()
     {
+        Transform[] allchild = GetComponentsInChildren<Transform>();
+
         if(!isSelect && isExecute)
         {
-            renderers.material = originalMat;
+            foreach(Transform child in allchild)
+            {
+                child.gameObject.GetComponent<Renderer>().material = originalMat;
+            }
             if (UIManager.instance.IsOpenedUI(UIType.InteractUI))
             {
                 UIManager.instance.CloseUI(UIType.InteractUI);
@@ -56,7 +67,9 @@ public class TreasureBox : MonoBehaviour
     void GetItem()
     {
         // 상자 열리는 애니메이션
+        animator.SetBool("IsOpen", true);
         // 아이템의 실제 오브젝트 생성
+
     }
 
 }
